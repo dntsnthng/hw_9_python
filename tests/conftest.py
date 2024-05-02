80dabc593a2d1f20f97d7c8ed5fd445faa3d07d9
Elmin Gulamov <g.elmn@yandex.ru>
2024-05-02 16:54:28 +0400

    conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,12 @@
 import pytest
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-
 from utils import attach
 
 
-# @pytest.fixture(scope="function", autouse=True)
-# def open_browser():
-#     browser.config.base_url = 'https://demoqa.com'
-#     driver_options = webdriver.ChromeOptions()
-#     driver_options.page_load_strategy = 'eager'
-#     browser.config.driver_options = driver_options
-#
-#     yield
-#
-#     browser.quit()
-
-
-
-@pytest.fixture(scope="function")
-def setup_browser(request):
+@pytest.fixture(scope='function', autouse=True)
+def configuration(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -36,7 +22,11 @@ def setup_browser(request):
         options=options
     )
 
-    browser = Browser(Config(driver))
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.driver = driver
+
     yield browser
 
     attach.add_screenshot(browser)
