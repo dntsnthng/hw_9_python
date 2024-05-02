365ed828bbab2d0fe6280b8641d7274af2953557
Elmin Gulamov <g.elmn@yandex.ru>
2024-05-02 16:49:57 +0400

    requirements.txt

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,18 +7,20 @@ from selene import Browser, Config
 from utils import attach
 
 
+# @pytest.fixture(scope="function", autouse=True)
+# def open_browser():
+#     browser.config.base_url = 'https://demoqa.com'
+#     driver_options = webdriver.ChromeOptions()
+#     driver_options.page_load_strategy = 'eager'
+#     browser.config.driver_options = driver_options
+#
+#     yield
+#
+#     browser.quit()
+
+
+
 @pytest.fixture(scope="function")
-def open_browser():
-    browser.config.base_url = 'https://demoqa.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-
-    yield
-
-    browser.quit()
-
-
 def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
